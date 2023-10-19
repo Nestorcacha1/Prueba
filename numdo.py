6e083ef87810833302c846676b43e9d442956955
Thrones <nestorcachacelmi.@gmail.com>
2023-10-19 09:52:11 -0500

    Verficacion

--- numdo.py
+++ numdo.py
@@ -3,3 +3,16 @@ a= 2
 b= 3
 c= a+b
 print("La suma de a y b es: ",c)
+#crea un rosa color rojo con turtle
+print("Ejemplo de turtle")
+import turtle
+t=turtle.Pen()  
+t.reset()
+t.color(1,0,0)
+for x in range(1,38):
+    t.forward(100)
+    t.left(175)
+turtle.getscreen()._root.mainloop()
+#crea un array de nueve elementos y listalo de mayor a menor
+
+
