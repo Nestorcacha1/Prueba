4369c15cedffdbeffcf2f5df2bfbbd9838748796
Thrones <nestorcachacelmi.@gmail.com>
2023-10-19 09:19:32 -0500

    Rosa nautica

--- index.py
+++ index.py
@@ -2,6 +2,15 @@ print("New Hello GIT")
 print("Bienvenido a GIT")
 print("1233")
 
+#crea una rosa nautica con turtle
+print("Ejemplo de turtle")
+import turtle
+t=turtle.Pen()
+t.reset()
+for x in range(1,38):
+    t.forward(100)
+    t.left(175)
+turtle.getscreen()._root.mainloop()
 
 
 
