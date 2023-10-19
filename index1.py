ea451c84041fb0122de83f87b5228d11bc3d8e9a
Thrones <nestorcachacelmi.@gmail.com>
2023-10-19 09:16:39 -0500

    Array de numeros

--- index1.py
+++ index1.py
@@ -1,2 +1,12 @@
 print("Bienvenido a GIT")
 print("Hello GIT")
+#crea un array de nueve elementos y listalo de mayor a menor
+#usando el metodo sort
+print("Ejemplo de sort")
+a=[1,2,3,4,5,6,7,8,9]
+a.sort(reverse=True)
+
+print(a)
+
+
+
